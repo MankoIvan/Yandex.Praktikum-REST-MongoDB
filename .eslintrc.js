dc9aff7ce951079acdfce8cabcb57a78af8fdb75
Manko Ivan <inoi2002@gmail.com>
2019-10-30 14:04:27 +0300

    commiting for review

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "commonjs": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-underscore-dangle": [
+        "error",
+        {
+          "allow": [
+            "_id",
+          ]
+        }
+      ]
+    }
+};
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { PORT = 3000 } = process.env;
+
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5db8ad51ca029d0d443298f1'
+  };
+  next();
+});
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+app.get('*', (req, res) => {
+  res.status(404);
+  res.send( { message: "Запрашиваемый ресурс не найден" } );
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
+
+
+/* 5db8ad51ca029d0d443298f1 */
